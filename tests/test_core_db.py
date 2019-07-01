97f4fc66bc7297ff7294301eefef528b3e4722a7
Nicolas Hainaux <nh.techn@gmail.com>
2019-07-01 18:01:22 +0200

    Update the tests.

--- tests/test_core_db.py
+++ tests/test_core_db.py
@@ -19,23 +19,33 @@
 # along with VocaShaker; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import pytest
 import sqlite3
+# from unittest.mock import patch
 
-from vocashaker.core.db import Connect
-# from vocashaker.core.db import table_exists
+import pytest
+
+# from vocashaker.core.env import TEST_DB_PATH
+# from vocashaker.core import shared
+from vocashaker.core.database import Manager
+# from vocashaker.core.database import list_tables
 
 
-def test_Connect():
-    with Connect(':memory:') as c:
+def test_Manager():
+    with Manager(':memory:') as db:
         cmd = """CREATE TABLE test1
                  (id INTEGER PRIMARY KEY, col1 INTEGER, col2 INTEGER)"""
-        c.execute(cmd)
+        db.execute(cmd)
     # Check that, out of the with statement, the connection is *closed*
     # (not only committed)
     with pytest.raises(sqlite3.ProgrammingError) as excinfo:
-        c.execute('SELECT 1 FROM test1;')
+        db.execute('SELECT 1 FROM test1;')
     assert str(excinfo.value) == 'Cannot operate on a closed database.'
 
-# def test_table_exists():
-#     pass
+
+# with Manager(TEST_DB_PATH) as db:
+#     @patch(shared.db, db)
+#     def test_list_tables():
+#         assert list_tables() == ['table1', 'table2']
+
+    # def test_table_exists():
+    #     pass
