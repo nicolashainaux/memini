dd1f832bb452b258c8af87cdb5085ad47ef118b4
Nicolas Hainaux <nh.techn@gmail.com>
2019-08-17 07:21:51 +0200

    Write tests for _process_data() in core/document.py

--- tests/test_core_document.py
+++ tests/test_core_document.py
@@ -22,7 +22,9 @@
 import pytest
 
 from vocashaker.core.errors import SchemeSyntaxError, SchemeLogicalError
+from vocashaker.core.errors import SchemeColumnsMismatchError
 from vocashaker.core.document import _default_scheme, _parse_scheme
+from vocashaker.core.document import _process_data
 
 
 def test_default_scheme():
@@ -58,3 +60,67 @@ def test_parse_scheme():
     assert _parse_scheme('**_') == ([2], 1)
     assert _parse_scheme('___') == ([0, 1, 2], 2)
     assert _parse_scheme('___1') == ([0, 1, 2], 1)
+
+
+def test_process_data(mocker, testdb):
+    data = [('adventus,  us, m.', 'arrivée'),
+            ('candidus,  a, um', 'blanc'),
+            ('sol, solis, m', 'soleil')]
+    scheme = '___'
+    with pytest.raises(SchemeColumnsMismatchError) as excinfo:
+        _process_data(data, scheme)
+    assert str(excinfo.value) == 'The provided scheme (**_2) does not have '\
+        'the same number of columns as the table (2).'
+
+    # Test default scheme
+    result = _process_data(data)
+    expected_answers = [{'col1': 'adventus,  us, m.', 'col2': 'arrivée'},
+                        {'col1': 'candidus,  a, um', 'col2': 'blanc'},
+                        {'col1': 'sol, solis, m', 'col2': 'soleil'}]
+    assert result['answers'] == expected_answers
+    assert len(result['rows']) == 3
+    assert ((result['rows'][0]['col1'] == 'adventus,  us, m.'
+             and result['rows'][0]['col2'] == '')
+            or (result['rows'][0]['col1'] == ''
+                and result['rows'][0]['col2'] == 'arrivée'))
+    assert ((result['rows'][1]['col1'] == 'candidus,  a, um'
+             and result['rows'][1]['col2'] == '')
+            or (result['rows'][1]['col1'] == ''
+                and result['rows'][1]['col2'] == 'blanc'))
+    assert ((result['rows'][2]['col1'] == 'sol, solis, m'
+             and result['rows'][2]['col2'] == '')
+            or (result['rows'][2]['col1'] == ''
+                and result['rows'][2]['col2'] == 'soleil'))
+
+    data = [('bieten', 'bot, hat geboten', 'offrir'),
+            ('bleiben', 'blieb, ist geblieben', 'rester'),
+            ('gelingen', 'gelang, ist gelungen', 'réussir'),
+            ('schmelzen', 'schmolz, ist geschmolzen', 'fondre'),
+            ('ziegen', 'zog, hat OU ist gezogen', 'tirer OU déménager'),
+            ]
+    scheme = '__*'
+    result = _process_data(data, scheme)
+    expected_answers = \
+        [{'col1': 'bieten', 'col2': 'bot, hat geboten', 'col3': 'offrir'},
+         {'col1': 'bleiben', 'col2': 'blieb, ist geblieben', 'col3': 'rester'},
+         {'col1': 'gelingen', 'col2': 'gelang, ist gelungen',
+          'col3': 'réussir'},
+         {'col1': 'schmelzen', 'col2': 'schmolz, ist geschmolzen',
+          'col3': 'fondre'},
+         {'col1': 'ziegen', 'col2': 'zog, hat OU ist gezogen',
+          'col3': 'tirer OU déménager'}]
+    assert result['answers'] == expected_answers
+    expected_rows = \
+        [{'col1': '', 'col2': '', 'col3': 'offrir'},
+         {'col1': '', 'col2': '', 'col3': 'rester'},
+         {'col1': '', 'col2': '', 'col3': 'réussir'},
+         {'col1': '', 'col2': '', 'col3': 'fondre'},
+         {'col1': '', 'col2': '', 'col3': 'tirer OU déménager'}]
+    assert result['rows'] == expected_rows
+
+    scheme = '__*1'
+    result = _process_data(data, scheme)
+    assert result['answers'] == expected_answers
+    for row in result['rows']:
+        assert list(row.keys()) == ['col1', 'col2', 'col3']
+        assert list(row.values()[:-1]).count('') == 1
