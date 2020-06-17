708034fdb2205f6c0644e89c261b5594557b51a3
Nicolas Hainaux <nh.techn@gmail.com>
2020-06-17 17:41:27 +0200

    Add core/dialog.py, the ask_yes_no() function and its test.

---
+++ tests/test_core_05_dialog.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# VocaShaker is a simple project that creates vocabulary grids to train.
+# Copyright 2019 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of VocaShaker.
+
+# VocaShaker is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# VocaShaker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with VocaShaker; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from unittest.mock import patch
+
+from vocashaker.core import dialog
+
+
+def test_ask_yes_no(capsys):
+    with patch('builtins.input') as mock_input:
+        mock_input.return_value = 'YES'
+        answer = dialog.ask_yes_no('Will you do it?')
+        assert answer
+        mock_input.assert_called_with('Will you do it? [y/N] ')
+
+        answer = dialog.ask_yes_no('Will you do it?', default=True)
+        assert answer
+        mock_input.assert_called_with('Will you do it? [Y/n] ')
+
+        values = ['Y', 'y', 'yes', 'Yes', 'yEs', 'yeS', 'YEs', 'yES', 'YeS']
+        mock_input.side_effect = values
+        for i in range(len(values)):
+            assert dialog.ask_yes_no('Will you do it?')
+
+        values = ['N', 'n', 'no', 'No', 'nO', 'NO']
+        mock_input.side_effect = values
+        for i in range(len(values)):
+            assert not dialog.ask_yes_no('Will you do it?')
+
+        mock_input.side_effect = ['', '']
+        assert not dialog.ask_yes_no('Will you do it?')
+        assert dialog.ask_yes_no('Will you do it?', default=True)
+
+        mock_input.side_effect = ['maybe', 'Y']
+        dialog.ask_yes_no('Will you do it?')
+        captured = capsys.readouterr()
+        assert captured.out == 'Sorry, I didn\'t understand.\n'
---
+++ vocashaker/core/dialog.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# VocaShaker is a simple project that creates vocabulary grids to train.
+# Copyright 2019 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of VocaShaker.
+
+# VocaShaker is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# VocaShaker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with VocaShaker; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+def ask_yes_no(question, default=False):
+    """
+    Ask the user to give a positive or negative answer to the given question.
+    Accepted answers are case insensitive 'yes', 'no', 'y' or 'n'.
+    If the user only types "enter" then the default answer is returned.
+    The default answer depends on the default keyword argument: False for "no",
+    True for "yes".
+    If any unaccepted answer is given, then the user is asked again.
+    """
+    result = None
+    yn = '[Y/n]' if default else '[y/N]'
+    while result is None:
+        answer = input('{} {} '.format(question, yn))
+        if answer.lower() in ['y', 'yes']:
+            result = True
+        elif answer.lower() in ['n', 'no']:
+            result = False
+        elif answer == '':
+            result = default
+        if result is None:
+            print('Sorry, I didn\'t understand.')
+    return result
